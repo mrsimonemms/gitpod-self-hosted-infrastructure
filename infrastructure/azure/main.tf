08f5105678fb33f5ab2d13ad7e14028990c94a92
Simon Emms <simon@simonemms.com>
2022-09-12 10:16:59 +0000

    fix(azure): add random number to resource group

--- infrastructure/azure/main.tf
+++ infrastructure/azure/main.tf
@@ -9,7 +9,12 @@ terraform {
 
 data "azurerm_client_config" "current" {}
 
+resource "random_integer" "resource_group" {
+  min = 1000
+  max = 9999
+}
+
 resource "azurerm_resource_group" "gitpod" {
-  name     = format(var.name_format_global, local.location)
+  name     = format(var.name_format_global, "${random_integer.resource_group.result}-${local.location}")
   location = var.location
 }
